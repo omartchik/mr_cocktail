1dade41a95a70031557393bb2441319cfd4ad4e9
omar rachedi <omartchik@gmail.com>
2020-11-30 23:59:27 +0100

    list cocktails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'cocktails/index'
+  get 'cocktails/show'
+  get 'cocktails/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
     resources :ingredients
